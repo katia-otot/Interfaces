5a18cde434d7546c4450bb62fa86d7d9c81f5ec6
katia-otot <katiagadea@gmail.com>
2024-10-16 20:29:09 -0300

    falta poner dificultad al juego

--- Base juego/main2.js
+++ Base juego/main2.js
@@ -3,7 +3,12 @@
 
 // Seleccionamos los botones
 const playButton = document.getElementById('playButton');
-const controlsButton = document.getElementById('controlsButton');
+// Obtener el modal
+const modal = document.getElementById("aboutModal");
+// Obtener el botón que abre el modal
+const aboutBtn = document.getElementById("aboutButton");
+// Obtener el elemento <span> que cierra el modal
+const closeBtn = document.querySelector(".close");
 
 // Acción al hacer clic en el botón de 'Play'
 playButton.addEventListener('click', () => {
@@ -16,7 +21,19 @@ playButton.addEventListener('click', () => {
     // Iniciar el ciclo del juego (gameLoop ya se ejecuta automáticamente)
 });
 
-// Acción al hacer clic en el botón de 'Controles'
-controlsButton.addEventListener('click', () => {
-    window.location.href = 'controls.html';  // Redirigimos a la página de controles
+// Cuando el usuario hace clic en el botón de About, se abre el modal
+aboutBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Cuando el usuario hace clic en la X, se cierra el modal
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Cuando el usuario hace clic en cualquier lugar fuera del modal, se cierra
+window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 });
